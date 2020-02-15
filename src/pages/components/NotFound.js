4b9b9470c63e751b62a9e8975d6e8d22dfe95a0c
Andrés Felipe Cano <25773235+AndresFelipeCano@users.noreply.github.com>
2020-02-15 10:02:14 -0500

    Update NotFound.js
    Navegación por eventos en la aplicación

--- src/pages/components/NotFound.js
+++ src/pages/components/NotFound.js
@@ -2,12 +2,41 @@ import React, { PureComponent } from 'react';
 import './Header';
 
 class NotFound extends PureComponent{
+
+  hanldeBackClick = () =>{
+    this.props.history.goBack();
+  }
+  hanldeForwardClick = () => {
+    this.props.history.goForward();
+  }
+  hanldeRandomClick = () => {
+    const random = Math.round(Math.random() * (10 - 1) + 1); // Número aleatorio entero entre 1 y 10
+    this.props.history.push(`/videos?id=${random}`, { id: random});
+  }
   render() {
     return (
       <div className="Page NotFound">
         <h1>404</h1>
         <h3 className="SadFace">:(</h3>
         <h2>No hemos encontrado la página que buscabas</h2>
+        <button 
+          className="Button"
+          onClick={this.hanldeForwardClick}
+        >
+          Ir a la siguiente ruta
+        </button>
+        <button 
+          className="Button"
+          onClick={this.hanldeBackClick}
+        >
+          Ir a la ruta anterior
+        </button>
+        <button 
+          className="Button"
+          onClick={this.hanldeRandomClick}
+        >
+          Video Random
+        </button>
       </div>
     )
   }
